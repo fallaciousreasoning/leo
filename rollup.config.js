0e3f39c7f6d703c4f270d2a056b05527aa2d7289
Jay Harris <jay.harris@outlook.co.nz>
2022-08-31 10:06:27 +1200

    Generate react files

--- rollup.config.js
+++ rollup.config.js
@@ -4,23 +4,27 @@ import sveltePreprocess from 'svelte-preprocess';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import fs from 'fs'
-import svelteDts from 'svelte-dts';
+import path from 'path'
 
-const webComponents = fs.readdirSync('./web-components').map(c => `./web-components/${c}/${c}.svelte`).filter(c => fs.existsSync(c));
+const webComponents = fs.readdirSync('./web-components')
+    .map(c => [`./web-components/${c}/${c}.svelte`, `./web-components/${c}/react.ts`])
+    .filter(([c]) => fs.existsSync(c))
+    .flatMap(c => c);
 
 export default {
     input: [...webComponents],
     output: {
         dir: 'build/web-components',
         // The destination for our bundled JavaScript
-        entryFileNames: '[name]/index.js',
+        entryFileNames: (file) => {
+            const directory = path.basename(path.resolve(file.facadeModuleId, '..'));
+            console.log(directory)
+            return `${directory}/${file.name.endsWith('react') ? 'react' : 'index'}.js`
+        },
         format: 'esm',
         sourcemap: true,
     },
     plugins: [
-        // svelteDts({
-        //     output: 'build/web-components/index.d.ts'
-        // }),
         svelte({
             compilerOptions: {
                 customElement: true
