68306bfc5e9c37d973d9b7c41575f02c17539c60
Jay Harris <jay.harris@outlook.co.nz>
2022-12-01 10:16:39 +1300

    Expose all variables

--- web-components/navdots/navdots.svelte
+++ web-components/navdots/navdots.svelte
@@ -42,21 +42,26 @@
 </nav>
 
 <style lang="scss">
-  :root {
-    --dot-size: 8px;
-    --dot-spacing: 10px;
-    --dot-vertical-margin: 1px;
-    --current-dot: 0;
-    --transition-duration: 0.2s;
-    --transition-easing: ease-in-out;
-
-    // Expanded dot grows to fill half spacing in each direction.
-    --expanded-dot-size: calc(var(--dot-size) + var(--dot-spacing));
-
-    width: 100%;
-  }
-
   .leo-navdots {
+    --transition-duration: var(--leo-navdots-transition-duration, 0.2s);
+    --transition-easing: var(--leo-navdots-transition-easing, ease-in-out);
+
+    --size: var(--leo-navdots-size, 8px);
+    --spacing: var(--leo-navdots-spacing, 10px);
+    --vertical-margin: var(--leo-navdots-vertical-margin, 1px);
+
+    // By default, expanded dot grows to fill half spacing in each direction.
+    --expanded-width: var(--leo-navdots-expanded-width, calc(var(--leo-navdots-size) + var(--leo-navdots-spacing)));
+
+    --color: var(--leo-navdots-color, var(--color-primary-20));
+    --color-hover: var(--leo-navdots-color-hover, var(--color-primary-30));
+
+    --active-color: var(--leo-navdots-active-color, var(--color-interaction-button-primary-background));
+    --active-color-hover: var(--leo-navdots-active-color-hover, var(--color-icon-interactive));
+    --active-outline-color: var(--leo-navdots-focus-outline-color, #423eee);
+
+    --current-dot: 0;
+
     display: flex;
     flex-direction: row;
     justify-content: center;
@@ -64,9 +69,9 @@
     .dot-container {
       display: flex;
       flex-direction: row;
-      gap: var(--dot-spacing);
+      gap: var(--spacing);
       position: relative;
-      padding: 0 calc(var(--dot-spacing) / 2);
+      padding: 0 calc(var(--spacing) / 2);
       margin: 0;
       list-style: none;
 
@@ -76,29 +81,29 @@
 
       &:has(.dot.active:focus-visible) .active-dot {
         box-shadow: 0px 0px 0px 1.5px rgba(255, 255, 255, 0.5),
-          0px 0px 4px 2px #423eee;
+          0px 0px 4px 2px var(--focus-outline-color);
       }
     }
 
     .dot {
       all: unset;
       cursor: pointer;
-      margin: var(--dot-vertical-margin) 0;
-      width: var(--dot-size);
-      height: var(--dot-size);
-      border-radius: var(--dot-size);
-      background: var(--color-primary-20);
+      margin: var(--vertical-margin) 0;
+      width: var(--size);
+      height: var(--size);
+      border-radius: var(--size);
+      background: var(--color);
       transition: background-color var(--transition-duration)
           var(--transition-easing),
         box-shadow var(--transition-duration) var(--transition-easing);
 
       &:hover {
-        background-color: var(--color-primary-30);
+        background-color: var(--color-hover);
       }
 
       &:focus-visible:not(.active) {
         box-shadow: 0px 0px 0px 1.5px rgba(255, 255, 255, 0.5),
-          0px 0px 4px 2px #423eee;
+          0px 0px 4px 2px var(--focus-outline-color);
       }
     }
 
@@ -109,18 +114,18 @@
         box-shadow var(--transition-duration) var(--transition-easing);
       transform: translate(
         calc(
-          (var(--dot-size) + var(--dot-spacing)) * var(--current-dot) -
-            var(--dot-spacing) / 2
+          (var(--size) + var(--spacing)) * var(--current-dot) -
+            var(--spacing) / 2
         ),
         0
       );
-      width: calc(var(--dot-size) + var(--dot-spacing));
-      height: calc(var(--dot-size) + var(--dot-vertical-margin) * 2);
-      border-radius: var(--dot-size);
-      background: var(--color-interaction-button-primary-background);
+      width: calc(var(--size) + var(--spacing));
+      height: calc(var(--size) + var(--vertical-margin) * 2);
+      border-radius: var(--size);
+      background: var(--active-color);
 
       &:hover {
-        background: var(--color-icon-interactive);
+        background: var(--active-color-hover);
       }
     }
   }
